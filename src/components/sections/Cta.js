b910f26e39dec13ffca1a89b7ecd5b6ec629c63f
Richard Mailand <richjvm15@gmail.com>
2021-03-10 16:16:14 -0600

    test

---
+++ src/components/sections/Cta.js
@@ -0,0 +1,57 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import Input from "../elements/Input";
+
+const propTypes = {
+  ...SectionProps.types,
+  split: PropTypes.bool,
+};
+
+const defaultProps = {
+  ...SectionProps.defaults,
+  split: false,
+};
+
+const Cta = ({ className, topOuterDivider, bottomOuterDivider, topDivider, bottomDivider, hasBgColor, invertColor, split, ...props }) => {
+  const outerClasses = classNames(
+    "cta section center-content-mobile reveal-from-bottom",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "cta-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider",
+    split && "cta-split"
+  );
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <div className="cta-slogan">
+            <h3 className="m-0">Have a question about our services?</h3>
+          </div>
+          <div className="cta-action" style={{ marginBottom: "-30px" }}>
+            <p style={{ color: "white", marginTop: "5px" }}>
+              Email: blanketyblank@gmail.com
+              <br />
+              Call: 763-221-8722
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Cta.propTypes = propTypes;
+Cta.defaultProps = defaultProps;
+
+export default Cta;
---
+++ src/components/sections/ServiceArea.js
@@ -0,0 +1,72 @@
+import React from "react";
+import classNames from "classnames";
+import { SectionTilesProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
+
+const propTypes = {
+  ...SectionTilesProps.types,
+};
+
+const defaultProps = {
+  ...SectionTilesProps.defaults,
+};
+
+const ServiceArea = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+  const outerClasses = classNames(
+    "testimonial section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "testimonial-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
+
+  const tilesClasses = classNames("tiles-wrap", pushLeft && "push-left");
+
+  const sectionHeader = {
+    title: "Service Area",
+    paragraph:
+      "Vitae aliquet nec ullamcorper sit amet risus nullam eget felis semper quis lectus nulla at volutpat diam ut venenatis tellus—in ornare.",
+  };
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div
+            className="hero-figure reveal-from-bottom illustration-element-01 center-content"
+            data-reveal-value="20px"
+            data-reveal-delay="800"
+          >
+            <a data-video="https://player.vimeo.com/video/174002812" href="#0" aria-controls="video-modal">
+              <Image className="has-shadow" src={require("./../../assets/images/service-area.PNG")} alt="Hero" width={896} height={504} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+ServiceArea.propTypes = propTypes;
+ServiceArea.defaultProps = defaultProps;
+
+export default ServiceArea;
