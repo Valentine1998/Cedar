ae6f2379643fd0ecf549bba9ae4a81fb4ae0b3d5
Valentine1998 <richjvm15@gmail.com>
2022-06-29 16:47:32 -0500

    test

---
+++ build/precache-manifest.53ecc128c53a47a4dccce106fb21085d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6650472a428ab2ec55246dcef649a75d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "382d7a2fb4986ad30bc9",
+    "url": "/static/css/2.4a3545e4.chunk.css"
+  },
+  {
+    "revision": "9a4dec9ee842a40cdc29",
+    "url": "/static/css/main.64b18b27.chunk.css"
+  },
+  {
+    "revision": "382d7a2fb4986ad30bc9",
+    "url": "/static/js/2.08748406.chunk.js"
+  },
+  {
+    "revision": "40d987c57e560292c66692c343835381",
+    "url": "/static/js/2.08748406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4dec9ee842a40cdc29",
+    "url": "/static/js/main.8b5e48d9.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.8b5e48d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea630fe81389fbceefbb",
+    "url": "/static/js/runtime-main.bf9f4589.js"
+  },
+  {
+    "revision": "7c8c9169ca524f0bfe61442368939940",
+    "url": "/static/media/cool-truck.7c8c9169.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f1198870abf4f0d4ed733fad4fa3663",
+    "url": "/static/media/fam.0f119887.jpeg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "7862b019f3f1802a8a1bd8210f632222",
+    "url": "/static/media/logo.7862b019.png"
+  },
+  {
+    "revision": "f440c067b2869369d03987d17508f35d",
+    "url": "/static/media/truck.f440c067.jpg"
+  }
+]);
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -28,7 +28,9 @@ const Footer = ({ className, topOuterDivider, topDivider, ...props }) => {
           </div>
           <div className="footer-bottom space-between text-xxs invert-order-desktop">
             <FooterNav />
-            <div className="footer-copyright">Made by Up North Web Development. All right reserved</div>
+            <div className="footer-copyright">
+              Made by <a href="https://cruip.com">Cruip</a>. All right reserved
+            </div>
           </div>
         </div>
       </div>
